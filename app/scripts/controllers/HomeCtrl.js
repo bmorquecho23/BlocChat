28f0cdf2097938d1af94ede1d685200ea41ce327
Billy Morquecho <billymorquecho@gmail.com>
2018-01-08 15:13:02 -0600

    checkpoint 6 complete

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,19 +1,25 @@
 (function() {
-    function HomeCtrl(Room, Message) {
+    function HomeCtrl(Room, Message, $cookies) {
       this.Rooms = Room.all;
       this.messages = null;
+      this.currentUser = $cookies.get('blocChatCurrentUser');
 
 
       this.selectRoom=function(room){
         this.currentRoom=room;
         this.messages=Message.getByRoomId(room.$id);
-        console.log(room.$id);
-        console.log(Message.getByRoomId(room.$id));
+      }
+
+      this.sendMessage = function(){
+        this.newMessage.roomId = this.currentRoom.$id;
+        this.newMessage.username = this.currentUser;
+        this.newMessage.sentAt = Date.now();
+        Message.send(this.newMessage);
       }
 
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room','Message', HomeCtrl]);
+        .controller('HomeCtrl', ['Room','Message','$cookies', HomeCtrl]);
 })();
